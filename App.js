f811beb465de3bd57998f7bea45f0114a183c158
Edemilton Alcides Galindo Junior <edemiltonjr@gmail.com>
2024-04-22 17:09:20 -0300

    Início de implementação de Botton Tab Navigator

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Login from './src/screens/Login'
 import CreateUser from './src/screens/CreateUser'
+import Home from './src/screens/Home';
+import About from './src/screens/About';
+import Account from './src/screens/Account';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="Tabs">
 
                 <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
 
                 <Stack.Screen name="CreateUser" component={CreateUser} />
 
+                <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+
             </Stack.Navigator>
         </NavigationContainer>
+    );
+}
+
+function Tabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Início" component={Home} />
+            <Tab.Screen name="Conta" component={Account} />
+            <Tab.Screen name="Sobre" component={About} />
+        </Tab.Navigator>
     );
 }
